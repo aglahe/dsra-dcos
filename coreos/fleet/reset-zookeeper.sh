92dcab017b34888c97ed49472346907e21e005d1
Matt Parker <parker20121@gmail.com>
2015-08-11 16:45:22 +0000

    Modifications to zookeeper unti files to work with new metadata

---
+++ coreos/fleet/reset-zookeeper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+fleetctl list-unit-files
+fleetctl destroy zookeeper{1..5}.service
+fleetctl submit zookeeper*.service 
+fleetctl start zookeeper{1..5}.service
+fleetctl list-unit-files
