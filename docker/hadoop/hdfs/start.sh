6bb2c0472251a2af33b490ac2249b8284ef65624
Aaron Glahe <aaron.glahe@gmail.com>
2015-08-28 12:51:04 -0400

    Bascially removed the specific version of the container.  Need to figure out a way to use ENTRYPOINT and CMD so that we can format, and then also, start the various parts.

--- docker/hadoop/hdfs/start.sh
+++ docker/hadoop/hdfs/start.sh
@@ -5,18 +5,23 @@ if [ -z $CLUSTER_NAME ]; then
   export CLUSTER_NAME
 fi
 
+# make sure we have everyrthing
 if [ -z $NNODE1_IP ] || [ -z $NNODE2_IP ]  || [ -z $ZK_IPS ] || [ -z $JN_IPS ]; then
   echo NNODE1_IP, NNODE2_IP, JN_IPS and ZK_IPS needs to be set as environment addresses to be able to run.
   exit;
 fi
 
+# convert the commas to semicolons
 JNODES=$(echo $JN_IPS | tr "," ";")
 
+# Replace all the variables
 sed "s/CLUSTER_NAME/$CLUSTER_NAME/" /usr/local/hadoop/etc/hadoop/hdfs-site.xml.template \
 | sed "s/NNODE1_IP/$NNODE1_IP/" \
 | sed "s/NNODE2_IP/$NNODE2_IP/" \
-| sed "s/ZKNODES/$ZK_IPS/" \
+| sed "s/ZK_IPS/$ZK_IPS/" \
 | sed "s/JNODES/$JNODES/" \
 > /usr/local/hadoop/etc/hadoop/hdfs-site.xml
 
 sed "s/CLUSTER_NAME/$CLUSTER_NAME/" /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
+
+eval $*
