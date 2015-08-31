d376ac309ce29b4df4ed6f89eee8ecfbce966d1c
Aaron Glahe <aaron.glahe@gmail.com>
2015-08-31 09:42:54 -0400

    1st cut of the new case statemen for starting up HDFS

--- docker/hadoop/hdfs/start.sh
+++ docker/hadoop/hdfs/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Check for cluster name
 if [ -z $CLUSTER_NAME ]; then
   CLUSTER_NAME="cluster"
   export CLUSTER_NAME
@@ -11,10 +12,10 @@ if [ -z $NNODE1_IP ] || [ -z $NNODE2_IP ]  || [ -z $ZK_IPS ] || [ -z $JN_IPS ]; 
   exit;
 fi
 
-# convert the commas to semicolons
+# convert the commas to semicolons..if they exist
 JNODES=$(echo $JN_IPS | tr "," ";")
 
-# Replace all the variables
+# Replace all the variables in hdfs-site.xml
 sed "s/CLUSTER_NAME/$CLUSTER_NAME/" /usr/local/hadoop/etc/hadoop/hdfs-site.xml.template \
 | sed "s/NNODE1_IP/$NNODE1_IP/" \
 | sed "s/NNODE2_IP/$NNODE2_IP/" \
@@ -22,6 +23,38 @@ sed "s/CLUSTER_NAME/$CLUSTER_NAME/" /usr/local/hadoop/etc/hadoop/hdfs-site.xml.t
 | sed "s/JNODES/$JNODES/" \
 > /usr/local/hadoop/etc/hadoop/hdfs-site.xml
 
+# Replace all the variables in core-site.xml
 sed "s/CLUSTER_NAME/$CLUSTER_NAME/" /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
 
-eval $*
+# Read the 1st arg, and based upon one of the five: format or bootstrap or start the particular service
+# NN and ZKFC stick together
+case "$1" in
+  format)
+    $HADOOP_PREFIX/bin/hdfs namenode -format $CLUSTER_NAME
+
+    # Test for zookeeper before doing this
+    if [echo ruok | nc $(echo $ZK_IPS | cut -d',' -f1 | cut -d':' -f1 ) 2181]
+      $HADOOP_PREFIX/bin/hdfs zkfc -formatZK
+    fi
+    ;;
+  bootstrap)
+    $HADOOP_PREFIX/bin/hdfs namenode -bootstrapStandby
+    ;;
+  journalnode)
+    $HADOOP_PREFIX/sbin/hadoop-daemon.sh start journalnode
+    ;;
+  namenode)
+    $HADOOP_PREFIX/sbin/hadoop-daemon.sh start namenode
+
+    # Test for zookeeper before doing this
+    if [echo ruok | nc $(echo $ZK_IPS | cut -d',' -f1 | cut -d':' -f1 ) 2181]
+      $HADOOP_PREFIX/sbin/hadoop-daemon.sh start zkfc
+    fi
+    ;;
+  datanode)
+    $HADOOP_PREFIX/sbin/hadoop-daemon.sh start datanode
+    ;;
+  *)
+    echo $"Usage: {format|bootstrap|journalnode|namenode|datanode}"
+    eval $*
+esac
