c1069535fb79bcfac1bd31727251320136f7296c
Matt Parker <parker20121@gmail.com>
2015-09-09 23:37:34 -0400

    Moving services around to accomodate the haproxy

--- docker/haproxy/run.sh
+++ docker/haproxy/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -t -i --restart=always -p 80:80 dsra/haproxy
+docker run -d -t -i --restart=always --name=haproxy -p 80:80 -p 8080:8080 dsra/haproxy  haproxy -f /usr/local/etc/haproxy/haproxy.cfg
