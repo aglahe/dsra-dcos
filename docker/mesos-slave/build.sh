6fe83f8855f46a8e008788bba6a34cb5b1c9c98e
Matt Parker <parker20121@gmail.com>
2015-09-15 08:51:48 -0400

    Added new mesos-slave configuration, including spark

---
+++ docker/mesos-slave/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t dsra/mesos-slave:0.23.0.1 .
---
+++ docker/mesos-slave/push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker tag dsra/mesos-slave:0.23.0.1 hub.dsra.local:5000/dsra/mesos-slave:0.23.0.1
+
+docker push hub.dsra.local:5000/dsra/mesos-slave:0.23.0.1
