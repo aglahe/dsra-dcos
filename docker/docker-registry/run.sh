7233aa3d0b9ffda7d87b13c182be91205eec5ffc
Matt Parker <parker20121@gmail.com>
2015-09-21 08:53:22 -0400

    Modified haproxy configuration to point to additional mesos uis

--- docker/docker-registry/run.sh
+++ docker/docker-registry/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -t -i --net=host --name docker-registry --restart=always -v /opt/registry/registry_config.yml:/etc/docker/registry/config.yml -v /opt/registry/auth:/auth -v /opt/registry/certs:/certs -v /opt/registry/data:/data -e SETTINGS_FLAVOR=local -e STORAGE_PATH=/data -e SEARCH_BACKEND=sqlalchemy -e LOGLEVEL=DEBUG registry:2.1.1
+docker run -d -t -i --net=host --name docker-registry --restart=always -v /opt/registry/registry_config.yml:/etc/docker/registry/config.yml -v /opt/registry/auth:/auth -v /opt/registry/certs:/certs -v /opt/registry/data:/data -e SETTINGS_FLAVOR=local -e STORAGE_PATH=/data -e LOGLEVEL=DEBUG registry:2.1.1
