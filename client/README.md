db455032b7e41c2e9a1394f2d1067fafa92b108a
Aaron Glahe <aaron.glahe@gmail.com>
2015-09-21 10:52:17 -0400

    No longer need hdfs-envs.
    Also, started to work on the dsra-client, issue #4.  First cut

---
+++ client/README.md
@@ -0,0 +1,8 @@
+# vagrant-memex
+DSRA Client Vagrant VM
+
+Please make sure that vbguest is installed (https://github.com/dotless-de/vagrant-vbguest/).  It helps keep the VirtuaalBoxGuestAdditions up to date, install on the cmd line:
+```
+vagrant plugin install vagrant-vbguest
+```
+When starting this on Linux, do not use your distribution's Vagrant package to install vagrant. Install from https://www.vagrantup.com to ensure you're using the latest version of Vagrant. Using old versions of Vagrant will result in errors.
---
+++ client/install_software.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# install the little things
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y wget
+sudo apt-get install -y git
+
+# Install Java
+sudo apt-get install -y openjdk-7-jdk
+sudo echo "JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64" >> /etc/environment
+
+# Now install golang
+sudo wget -q -O - https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz | tar -xzf - -C /usr/local
+sudo echo "export GOROOT=/usr/local/go" >> /etc/profile.d/go.sh
+sudo echo "export PATH=\$GOROOT/bin:\$PATH" >> /etc/profile.d/go.sh
+
+# Get Hadoop, and "install" it
+sudo wget -q -O - http://apache.mirrors.pair.com/hadoop/common/hadoop-2.7.1/hadoop-2.7.1.tar.gz | tar -xzf - -C /usr/local
+sudo /usr/sbin/groupadd -r hdfs
+sudo /usr/sbin/groupadd -r hadoop
+sudo /usr/sbin/useradd -r -g hdfs -G hadoop hdfs
+sudo /bin/chown hdfs.hadoop -R /usr/local/hadoop-2.7.1
+sudo /bin/ln -s /usr/local/hadoop-2.7.1 /usr/local/hadoop
+sudo echo "export HADOOP_HOME=/usr/local/hadoop:\$PATH" >> /etc/profile.d/hdfs.sh
+sudo echo "export PATH=\$HADOOP_HOME/bin:\$PATH" >> /etc/profile.d/hdfs.sh
+
+# Get Docker installed
+sudo curl -sSL https://get.docker.com/ | sh
+
+# Now get fleetctl and etcdctl and set them up
+sudo git clone https://github.com/coreos/fleet.git /usr/local/fleet
+sudo git clone https://github.com/coreos/etcd.git /usr/local/etcd
+sudo /bin/bash /etc/profile.d/go.sh
+sudo /usr/local/fleet/build
+sudo /usr/local/etcd/build
+sudo echo "export PATH=/usr/local/etcd/bin:\$PATH" >> /etc/profile.d/etcd.sh
+sudo echo "export PATH=/usr/local/fleet/bin:\$PATH" >> /etc/profile.d/fleet.sh
+
+# Get Scala 2.11.7
+sudo wget -q -O - http://www.scala-lang.org/files/archive/scala-2.11.7.tgz | tar -xzf - -C /usr/lib
+sudo ln -s /usr/lib/scala-2.11.7 /usr/lib/scala
+sudo echo "export SCALA_HOME=/usr/lib/scala" >> /etc/profile.d/scala.sh
+sudo echo "export PATH=\$SCALA_HOME/bin:\$PATH" >> /etc/profile.d/scala.sh
+
+# Get MiniConda
+sudo wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
+sudo /bin/chmod 755 ./Miniconda-latest-Linux-x86_64.sh
+sudo ./Miniconda-latest-Linux-x86_64.sh -b -p /usr/local/anaconda
+sudo echo "export PATH=/usr/local/anaconda/bin:\$PATH" >> /etc/profile.d/anaconda.sh
+sudo /usr/local/anaconda/bin/conda update --yes --all
+sudo /usr/local/anaconda/bin/conda install --yes ipython
+sudo /usr/local/anaconda/bin/conda install --yes ipython-notebook
