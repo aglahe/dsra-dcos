5a156d685ef4ecc5b3fb8d48b88537cafa8b3cdf
Matt Parker <parker20121@gmail.com>
2015-10-14 10:06:04 -0400

    Upgrades mesos master and slave to 0.25.0-0.2.70.ubuntu1404

--- docker/mesos-master/build.sh
+++ docker/mesos-master/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t hub.dsra.local:5000/dsra/mesos-master:0.24.1-0.2.35.ubuntu1404 .
+docker build -t hub.dsra.local:5000/dsra/mesos-master:0.25.0-0.2.70.ubuntu1404 .
--- docker/mesos-master/push.sh
+++ docker/mesos-master/push.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# docker tag dsra/mesos-master:0.24.1-0.2.35.ubuntu1404 hub.dsra.local:5000/dsra/mesos-master:0.24.1-0.2.35.ubuntu1404
-
-docker push hub.dsra.local:5000/dsra/mesos-master:0.24.1-0.2.35.ubuntu1404
+docker push hub.dsra.local:5000/dsra/mesos-master:0.25.0-0.2.70.ubuntu1404
 
