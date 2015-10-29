e464a84063d3641586d15986b308669ea7cf0133
Aaron Glahe <aaron.glahe@gmail.com>
2015-10-29 11:38:24 -0400

    Updated to use bootstrap.sh and replacement in ini

--- docker/dsra-hue/bootstrap.sh
+++ docker/dsra-hue/bootstrap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # make sure we have everyrthing
-if [ -z $CLUSTER_NAME ] || [ -z $HTTPFS_SERVER ]  || [ -z $HTTPFS_PORT ]; then
+if [ -z $CLUSTER_NAME ] || [ -z $HTTPFS_SERVER ] || [ -z $HTTPFS_PORT ]; then
   echo CLUSTER_NAME, HTTPFS_SERVER, and HTTPFS_PORT needs to be set as environment addresses to be able to run.
   exit;
 fi
 
-# Replace all the variables in hdfs-site.xml
+# Replace all the variables in hue.ini.template
 sed "s/CLUSTER_NAME/$CLUSTER_NAME/" /tmp/hue.ini.template \
 | sed "s/HTTPFS_SERVER/$HTTPFS_SERVER/" \
 | sed "s/HTTPFS_PORT/$HTTPFS_PORT/" \
