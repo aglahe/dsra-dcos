4b3b08b2f479387df280edd83386c2c8c61ddcac
Aaron Glahe <aaron.glahe@gmail.com>
2015-10-30 11:16:04 -0400

    wrong mesos-dns entry..updated to corect FQDN

--- docker/dsra-hue/run.sh
+++ docker/dsra-hue/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -t -i --rm --name=hue -p 8000:8000 -e CLUSTER_NAME=dsra -e HTTPFS_SERVER=httpfs.dsra.local -e HTTPFS_PORT=32000 hub.dsra.local:5000/dsra/hue:3.9.0 start
+docker run -t -i --rm --name=hue -p 8000:8000 -e CLUSTER_NAME=dsra -e HTTPFS_SERVER=httpfs.marathon.mesos -e HTTPFS_PORT=32000 hub.dsra.local:5000/dsra/hue:3.9.0 start
