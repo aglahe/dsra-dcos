de93c26e79acd16d2118e1c7678248ee78d0f078
Matt Parker <parker20121@gmail.com>
2015-12-23 14:55:18 -0500

    haproxy template '

--- docker/docker-registry/run.sh
+++ docker/docker-registry/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -t -i --name registry -p 10.105.255.249:5000:5000 --restart=always -v /opt/registry/data:/data -v /opt/registry/index/docker-registry.db:/tmp/docker-registry.db -v /opt/registry/auth:/auth -v /opt/registry/certs:/certs -v /opt/registry/registry_config.yml:/etc/docker/registry/config.yml \registry:2.1.1
+docker run -d -t -i --name registry -p 10.105.255.249:5000:5000 --restart=always -v /opt/registry/data:/data -v /opt/registry/index/docker-registry.db:/tmp/docker-registry.db -v /opt/registry/auth:/auth -v /opt/registry/certs:/certs -v /opt/registry/registry_config.yml:/etc/docker/registry/config.yml registry:2.1.1
