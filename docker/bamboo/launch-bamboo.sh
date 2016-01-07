2d3d3276ae9200a25126b9d7be802947bbcbfd96
Matt Parker <parker20121@gmail.com>
2016-01-07 12:29:05 +0000

    Updated launch script

--- docker/bamboo/launch-bamboo.sh
+++ docker/bamboo/launch-bamboo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+docker stop bamboo
+
+docker rm -v bamboo
+
 docker run --name bamboo -t --rm -p 8000:8000 -p 80:80 \
+                               -v /home/core/dsra-dcos/docker/bamboo/haproxy_template.cfg:/opt/go/src/github.com/QubitProducts/bamboo/config/haproxy_template.cfg \
                                -e MARATHON_ENDPOINT=http://10.105.0.1,http://10.105.0.3,http://10.105.0.5,http://10.105.0.7,http://10.105.0.9 \
                                -e BAMBOO_ENDPOINT=http://localhost:8000 \
                                -e BAMBOO_ZK_HOST=zk://10.105.0.1:2181,10.105.0.3:2181,10.105.0.5:2181,10.105.0.7:2181,10.105.0.9:2181 \
@@ -7,5 +13,4 @@ docker run --name bamboo -t --rm -p 8000:8000 -p 80:80 \
                                -e BIND=":8000" \
                                -e CONFIG_PATH="config/production.example.json" \
                                -e BAMBOO_DOCKER_AUTO_HOST=true \
-                               hub.dsra.local:5000/dsra/bamboo:0.2.16.2
-
+                               hub.dsra.local:5000/dsra/bamboo:0.2.16.4
