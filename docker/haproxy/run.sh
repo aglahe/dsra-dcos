966e07d3e31a62d037d650bf6921193c822b23e8
Matt Parker <parker20121@gmail.com>
2016-01-07 17:01:36 -0500

    Modified haproxy configuration with load balancing for haproxy/bamboo setup

--- docker/haproxy/run.sh
+++ docker/haproxy/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -t -i --restart=always --name=haproxy --net=host -p 80:80 -p 8080:8080 dsra/haproxy  haproxy -f /usr/local/etc/haproxy/haproxy.cfg
+docker run -d -t -i --restart=always --name=haproxy --net=host -p 80:80 -p 8080:8080 hub.dsra.local:5000/dsra/haproxy:0.3 haproxy -f /usr/local/etc/haproxy/haproxy.cfg
